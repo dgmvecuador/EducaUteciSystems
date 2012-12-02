07cb1eb73dd09299cf2c462e731c13c203dd8225
Guillermo Pazos <shadowguiller@hotmail.com>
2012-12-01 20:13:44 -0500

    Se crean métodos para el despliegue de mensajes.

--- src/org/educautecisystems/core/Sistema.java
+++ src/org/educautecisystems/core/Sistema.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.Map;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.swing.JOptionPane;
 import org.educautecisystems.Esquemas.Ingreso;
 import org.educautecisystems.intefaz.VentanaPrincipal;
 
@@ -17,6 +18,17 @@ import org.educautecisystems.intefaz.VentanaPrincipal;
  */
 public class Sistema {
     private static EntityManagerFactory emf = null;
+    
+    /* Constantes */
+    public static final int VERSION_MAYOR = 1;
+    public static final int VERSION_MENOR = 0;
+    public static final int VERSION_PARCHE = 0;
+    public static final String NOMBRE_PROGRAMA = "EducaUteciSystems";
+    
+    public static String dameVersionCompleta () {
+        return NOMBRE_PROGRAMA+"-"+VERSION_MAYOR+"."+VERSION_MENOR+"."+VERSION_PARCHE;
+    }
+    
     public static void main( String []args ) {
         String usuario = "root";
         String password = "admin";
@@ -64,5 +76,13 @@ public class Sistema {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(Ingreso.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+    }
+    
+    public static void mostrarMensajeInformativo( String mensaje ) {
+        JOptionPane.showMessageDialog(null, mensaje, "Información - "+dameVersionCompleta(), JOptionPane.INFORMATION_MESSAGE);
+    }
+    
+    public static void mostrarMensajeError( String mensaje ) {
+        JOptionPane.showMessageDialog(null, mensaje, "Error - "+dameVersionCompleta(), JOptionPane.ERROR_MESSAGE);
     }
 }
