65cc007545a06f56f3b70adc0e8e2c7d7bac918c
DgMvEcuador <dgmvecuador@gmail.com>
2013-10-29 21:39:53 -0500

    Se corrige el nombre de una tabla de union.

--- src/org/educautecisystems/entidades/Docente.java
+++ src/org/educautecisystems/entidades/Docente.java
@@ -47,7 +47,7 @@ public class Docente implements Serializable {
     @Basic(optional = false)
     @Column(name = "Contrasena")
     private String contrasena;
-    @JoinTable(name = "docente_has_facultad", joinColumns = {
+    @JoinTable(name = "Docente_has_Facultad", joinColumns = {
         @JoinColumn(name = "Docente_Id_Docente", referencedColumnName = "Id_Docente")}, inverseJoinColumns = {
         @JoinColumn(name = "Facultad_Id_Facultad", referencedColumnName = "Id_Facultad")})
     @ManyToMany
